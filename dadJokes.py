d7aa97d09287880bdbcb4e8ee089c8151e8a0014
Colten Gaines <coltengaines@CGainesMBP.local>
2023-01-27 12:42:36 -0700

    Default input

--- dadJokes.py
+++ dadJokes.py
@@ -7,27 +7,25 @@ from random import choice
 def displayAscii(input="BA DUM TSS"):
     ascii_art = pyfiglet.figlet_format(input)
     colored_ascii = termcolor.colored(ascii_art, color="magenta")
-    print(colored_ascii)
-# Get the user input
-user_input = input("What would you like to search for? ")
+    return colored_ascii
+
 # Get the jokes from the API
-url = "https://icanhazdadjoke.com/search"
-response = requests.get(
-    url,
-    headers={"Accept": "application/json"},
-    params={"term": user_input}
-)
-data = response.json()
-num_jokes = data["total_jokes"]
-results = data["results"]
-if num_jokes == 0:
-    print(f"Sorry, I don't have any jokes about {user_input}! Please try again.")
-elif num_jokes == 1:
-    print(f"I've got one joke about {user_input}. Here it is:")
-    print(results[0]["joke"])
-    displayAscii()
-# If there are multiple jokes, print a random one
-else:
-    print(f"I've got {num_jokes} jokes about {user_input}. Here's one:")
-    print(choice(results)["joke"])
-    displayAscii()
+def getDadJoke(input=None):
+    url = "https://icanhazdadjoke.com/search"
+    response = requests.get(
+        url,
+        headers={"Accept": "application/json"},
+        params={"term": input}
+    )
+    data = response.json()
+    num_jokes = data["total_jokes"]
+    results = data["results"]
+    if num_jokes == 0:
+        return (f"Sorry, I don't have any jokes about {input}! Please try again.")
+    elif num_jokes == 1:
+        print(f"I've got one joke about {input}. Here it is:")
+        return results[0]["joke"]
+    # If there are multiple jokes, print a random one
+    else:
+        print(f"I've got {num_jokes} jokes about {input}. Here's one:")
+        return choice(results)["joke"]
